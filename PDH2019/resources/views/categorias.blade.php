f66a60a80e914fed96325a568a8966bd115079a8
verraca <veronica.racca@gmail.com>
2019-12-12 13:35:51 -0300

    Fix Buscador

--- PDH2019/resources/views/categorias.blade.php
+++ PDH2019/resources/views/categorias.blade.php
@@ -1,28 +1,36 @@
 @extends('master')
 
 @section('content')
-  <h1>{{$category->name}}</h1>
-  @forelse ($products as $product)
-    <article class="">
-       <div class="card mb-1 articulos" style="width: 85%;">
-  <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="/storage/product/{{$product->featuredimg}}" class="card-img" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h4 class="card-title"><a href="/producto/{{$product->id}}">{{$product->name}}</a></h4>
-        <p class="card-text">{{$product->description}}</p>
-        <h5 class="card-text">Precio: {{$product->price}}$</h5>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+@if (isset($category))
+
+<h1>{{$category->name}}</h1>
+
+@endif
+
+@forelse ($products as $product)
+<article class="">
+  <div class="card mb-1 articulos" style="width: 85%;">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img src="/storage/product/{{$product->featuredimg}}" class="card-img" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h4 class="card-title"><a href="/producto/{{$product->id}}">{{$product->name}}</a></h4>
+          <p class="card-text">{{$product->description}}</p>
+          <h5 class="card-text">Precio: {{$product->price}}$</h5>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
       </div>
     </div>
   </div>
-</div>
-    </article>
+</article>
 
-  @empty
-    <p>No hay productos disponibles</p>
-  @endforelse
+@empty
+<p>No hay productos disponibles</p>
+@endforelse
 
-  @endsection
+
+
+@endsection
