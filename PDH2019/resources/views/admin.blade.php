7dc96a266cdb51b03ffd5bb2178947137c0e8cf8
Luli Batlle <batllelucila@gmail.com>
2019-12-17 02:33:49 -0300

    Carrito

--- PDH2019/resources/views/admin.blade.php
+++ PDH2019/resources/views/admin.blade.php
@@ -1,16 +1,24 @@
 @extends('master')
 @section('content')
-  <h1>Hola Soy admin</h1>
+  <h1>Productos</h1>
   <ul>
 
 
 @foreach ($products as $product)
-  <li>{{$product->name}}</li>
+  <ul class="list-unstyled carritoentero">
+    <li class="media">
   <form class="" action="borrarproducto" method="post">
     @csrf
+          <div class="media-body">
+    <h2>{{$product->name}}</h2>
     <input type="hidden" name="id" value="{{$product->id}}">
     <button type="submit" class="btn btn-danger" name="button">Eliminar</button>
   </form>
+
+  </div>
+</li>
+</ul>
+
 @endforeach
   </ul>
 @endsection
--- PDH2019/resources/views/carrito.blade.php
+++ PDH2019/resources/views/carrito.blade.php
@@ -1,32 +1,36 @@
 @extends('master')
 
 @section('content')
-<div class="container">
-    <h1>Mi Carrito</h1>
-    <h2>Lista de productos</h2>
-
-    @forelse ($items as $item)
-        <article class="d-flex flex-row bd-highlight mb-3">
-          <img class="p-2 bd-highlight imagencarrito" src="/storage/product/{{$item->featuredimg}}" alt="">
-          <h4 class="name p-2 bd-highlight">{{$item->name}}</h4>
-          <p class="description p-2 bd-highlight">{{$item->description}}</p>
-          <p class="price p-2 bd-highlight">Precio: {{$item->subtotal}}$</p>
-  <form class="" action="/deletecart" method="post">
-    @csrf
-    <input  type="hidden" name="id" value="{{$item->id}}">
-    <button type="submit" class=" btn btn-danger col-xl-10 botoncarrito row">Eliminar</a>
-  </form>
-</article>
-      @empty
-
-      @endforelse
-      <form class="" action="/checkout" method="post">
-            @csrf
-        <input  type="hidden" name="id" value="">
-        <button type="submit" name="button">Comprar</button>
-      </form>
-
-
-
+<div class="cajacarrito">
+<ul class="listacarrito">
+    <li><h5>Producto</h5></li>
+    <li><h5>Subtotal</h5></li>
+    </ul>
+    <hr width=1200>
+      @forelse ($items as $item)
+        <div class="productoscarrito">
+<img src="/storage/product/{{$item->featuredimg}}" class="imagencarrito col-1" alt="...">
+      <h5 class="col-4">{{$item->name}}</h5>
+<h5 class="col-2 preciocarrito">${{$item->subtotal}}</h5>
+<form class="" action="/deletecart" method="post">
+  @csrf
+  <input  type="hidden" name="id" value="{{$item->id}}">
+  <button type="submit" class="btn btn-danger botoneliminar">Eliminar</a></button>
+</form>
 </div>
+    <hr width=1200>
+
+@empty
+
+@endforelse
+<hr width=1200>
+    <li class="totalcarrito"><h5>Total del carrito</h5></li>
+<hr width=1200>
+<p>ACA VA EL TOTAL ETC</p>
+<form class="" action="/checkout" method="post">
+      @csrf
+  <input  type="hidden" name="id" value="">
+  <button class="botoncomprar btn btn-warning" type="submit" name="button"><strong>FINALIZAR COMPRA</strong></button>
+</form>
+    </div>
 @endsection
